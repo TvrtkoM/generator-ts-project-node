9f7a1534ba98b90b0679e2a5224520118ab2804e
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2021-02-01 18:51:39 +0100

    Add express option for framework

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,21 @@
 const Generator = require('yeoman-generator');
 
+function getDevDeps(framework) {
+  let deps = ['eslint', '@typescript-eslint/parser', '@typescript-eslint/eslint-plugin', 'dotenv'];
+  if (framework === 'express') {
+    deps = [...deps, '@types/express', 'nodemon', 'ts-node'];
+  }
+  return deps;
+}
+
+function getDeps(framework) {
+  let deps = ['typescript'];
+
+  if (framework === 'express') {
+    deps = [...deps, 'express'];
+  }
+}
+
 module.exports = class extends Generator {
   async prompting() {
     this.appSettings = await this.prompt([
@@ -19,25 +35,45 @@ module.exports = class extends Generator {
         message: 'Application author',
       },
       {
+        type: 'list',
+        name: 'framework',
+        message: 'Plan to use a framework maybe?',
+        choices: [
+          {
+            name: 'None',
+            value: 'none',
+          },
+          {
+            name: 'Express.js',
+            value: 'express',
+          },
+        ],
+        default: 'none',
+      },
+      {
         type: 'confirm',
         name: 'initializeGit',
-        message: 'Initialize git repository?'
-      }
+        message: 'Initialize git repository?',
+      },
     ]);
   }
 
   writeFiles() {
-    this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'), {
+    this.fs.copyTpl(this.templatePath('package.json.ejs'), this.destinationPath('package.json'), {
       name: this.appSettings.name,
       author: this.appSettings.author,
       description: this.appSettings.description,
+      framework: this.appSettings.framework,
     });
 
     this.fs.copy(this.templatePath('.eslint*'), this.destinationPath());
     this.fs.copy(this.templatePath('.prettierrc.json'), this.destinationPath('.prettierrc.json'));
     this.fs.copy(this.templatePath('tsconfig.json'), this.destinationPath('tsconfig.json'));
 
-    this.fs.write(this.destinationPath('src/index.ts'), '');
+    this.fs.copyTpl(this.templatePath('index.ts.ejs'), this.destinationPath('src/index.ts'), {
+      framework: this.appSettings.framework,
+    });
+
     this.fs.write(this.destinationPath('.env'), '');
   }
 
@@ -49,8 +85,8 @@ module.exports = class extends Generator {
   }
 
   installDependencies() {
-    this.npmInstall(['typescript']);
-    this.npmInstall(['eslint', '@typescript-eslint/parser', '@typescript-eslint/eslint-plugin', 'dotenv'], {
+    this.npmInstall(getDeps(this.appSettings.framework));
+    this.npmInstall(getDevDeps(this.appSettings.framework), {
       'save-dev': true,
     });
   }
